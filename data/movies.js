21dd30135a89188276dc11d93d9e85f4cb87ecf6
Kamran <kamran.raja@hotmail.co.uk>
2025-02-07 18:05:27 +0000

    movies data added

---
+++ data/movies.js
@@ -0,0 +1,502 @@
+export default [
+  {
+    "title": "Back to the Future",
+    "year": 1985,
+    "cast": ["Michael J. Fox", "Christopher Lloyd"],
+    "director": "Robert Zemeckis",
+    "genre": ["Adventure", "Comedy", "Sci-Fi"],
+    "runtime": "116 min",
+    "certificate": "PG",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZmM3ZjE0NzctNjBiOC00MDZmLTgzMTUtNGVlOWFlOTNiZDJiXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "E.T. the Extra-Terrestrial",
+    "year": 1982,
+    "cast": ["Henry Thomas", "Drew Barrymore"],
+    "director": "Steven Spielberg",
+    "genre": ["Family", "Sci-Fi"],
+    "runtime": "115 min",
+    "certificate": "PG",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYTNhNmY0YWMtMTczYi00MTA0LThhMmUtMTIxYzE0Y2QwMzRlXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Breakfast Club",
+    "year": 1985,
+    "cast": ["Emilio Estevez", "Molly Ringwald"],
+    "director": "John Hughes",
+    "genre": ["Comedy", "Drama"],
+    "runtime": "97 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZTZiMGU1MWMtNjk0Yi00ZjNjLTljMDEtMDhkNGE2OWY3YzZiXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Die Hard",
+    "year": 1988,
+    "cast": ["Bruce Willis", "Alan Rickman"],
+    "director": "John McTiernan",
+    "genre": ["Action", "Thriller"],
+    "runtime": "132 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMGNlYmM1NmQtYWExMS00NmRjLTg5ZmEtMmYyYzJkMzljYWMxXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Shining",
+    "year": 1980,
+    "cast": ["Jack Nicholson", "Shelley Duvall", "Danny Lloyd"],
+    "director": "Stanley Kubrick",
+    "genre": ["Horror", "Drama"],
+    "runtime": "146 min",
+    "certificate": "R",
+    "poster_url": "https://image.tmdb.org/t/p/original/fFYAlrOudDJRYs8tvuHbUk0OGdL.jpg"
+  },
+  {
+    "title": "Ferris Bueller's Day Off",
+    "year": 1986,
+    "cast": ["Matthew Broderick", "Alan Ruck"],
+    "director": "John Hughes",
+    "genre": ["Comedy"],
+    "runtime": "103 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZWYwMjUxNjMtMzE0MC00NDM3LWIxMmQtYmEyNWVjNjdlZGZjXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Ghostbusters",
+    "year": 1984,
+    "cast": ["Bill Murray", "Dan Aykroyd"],
+    "director": "Ivan Reitman",
+    "genre": ["Action", "Comedy", "Fantasy"],
+    "runtime": "105 min",
+    "certificate": "PG",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMGI0Yjg2ODAtNDYzNi00Njc2LTlkMmMtMmRmYWI5MDE4ZGRkXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Blade Runner",
+    "year": 1982,
+    "cast": ["Harrison Ford", "Rutger Hauer"],
+    "director": "Ridley Scott",
+    "genre": ["Sci-Fi", "Thriller"],
+    "runtime": "117 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Terminator",
+    "year": 1984,
+    "cast": ["Arnold Schwarzenegger", "Linda Hamilton"],
+    "director": "James Cameron",
+    "genre": ["Action", "Sci-Fi"],
+    "runtime": "107 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZmE0YzIxM2QtMGNlMi00MjRmLWE3MWMtOWQzMGVjMmU0YTFmXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Stand by Me",
+    "year": 1986,
+    "cast": ["Wil Wheaton", "River Phoenix"],
+    "director": "Rob Reiner",
+    "genre": ["Adventure", "Drama"],
+    "runtime": "89 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNGJkNTUwM2MtZWE2MS00YjNkLWE3MjktNWQwZDQxMDIyZWVmXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Pulp Fiction",
+    "year": 1994,
+    "cast": ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+    "director": "Quentin Tarantino",
+    "genre": ["Crime", "Drama"],
+    "runtime": "154 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYTViYTE3ZGQtNDBlMC00ZTAyLTkyODMtZGRiZDg0MjA2YThkXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Shawshank Redemption",
+    "year": 1994,
+    "cast": ["Tim Robbins", "Morgan Freeman"],
+    "director": "Frank Darabont",
+    "genre": ["Drama"],
+    "runtime": "142 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Forrest Gump",
+    "year": 1994,
+    "cast": ["Tom Hanks", "Robin Wright"],
+    "director": "Robert Zemeckis",
+    "genre": ["Drama", "Romance"],
+    "runtime": "142 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNDYwNzVjMTItZmU5YS00YjQ5LTljYjgtMjY2NDVmYWMyNWFmXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Matrix",
+    "year": 1999,
+    "cast": ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+    "director": "Lana Wachowski, Lilly Wachowski",
+    "genre": ["Action", "Sci-Fi"],
+    "runtime": "136 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Jurassic Park",
+    "year": 1993,
+    "cast": ["Sam Neill", "Laura Dern", "Jeff Goldblum"],
+    "director": "Steven Spielberg",
+    "genre": ["Adventure", "Sci-Fi"],
+    "runtime": "127 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_SX300.jpg"
+  },
+  {
+    "title": "Goodfellas",
+    "year": 1990,
+    "cast": ["Robert De Niro", "Ray Liotta", "Joe Pesci"],
+    "director": "Martin Scorsese",
+    "genre": ["Biography", "Crime", "Drama"],
+    "runtime": "146 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BN2E5NzI2ZGMtY2VjNi00YTRjLWI1MDUtZGY5OWU1MWJjZjRjXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Fight Club",
+    "year": 1999,
+    "cast": ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
+    "director": "David Fincher",
+    "genre": ["Drama"],
+    "runtime": "139 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BOTgyOGQ1NDItNGU3Ny00MjU3LTg2YWEtNmEyYjBiMjI1Y2M5XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Silence of the Lambs",
+    "year": 1991,
+    "cast": ["Jodie Foster", "Anthony Hopkins"],
+    "director": "Jonathan Demme",
+    "genre": ["Crime", "Drama", "Thriller"],
+    "runtime": "118 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNDdhOGJhYzctYzYwZC00YmI2LWI0MjctYjg4ODdlMDExYjBlXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Saving Private Ryan",
+    "year": 1998,
+    "cast": ["Tom Hanks", "Matt Damon", "Tom Sizemore"],
+    "director": "Steven Spielberg",
+    "genre": ["Drama", "War"],
+    "runtime": "169 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZGZhZGQ1ZWUtZTZjYS00MDJhLWFkYjctN2ZlYjE5NWYwZDM2XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Titanic",
+    "year": 1997,
+    "cast": ["Leonardo DiCaprio", "Kate Winslet"],
+    "director": "James Cameron",
+    "genre": ["Drama", "Romance"],
+    "runtime": "195 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYzYyN2FiZmUtYWYzMy00MzViLWJkZTMtOGY1ZjgzNWMwN2YxXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Dark Knight",
+    "year": 2008,
+    "cast": ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    "director": "Christopher Nolan",
+    "genre": ["Action", "Crime", "Drama"],
+    "runtime": "152 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg"
+  },
+  {
+    "title": "Gladiator",
+    "year": 2000,
+    "cast": ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+    "director": "Ridley Scott",
+    "genre": ["Action", "Adventure", "Drama"],
+    "runtime": "155 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYWQ4YmNjYjEtOWE1Zi00Y2U4LWI4NTAtMTU0MjkxNWQ1ZmJiXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Lord of the Rings: The Return of the King",
+    "year": 2003,
+    "cast": ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
+    "director": "Peter Jackson",
+    "genre": ["Action", "Adventure", "Drama"],
+    "runtime": "201 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Memento",
+    "year": 2000,
+    "cast": ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano"],
+    "director": "Christopher Nolan",
+    "genre": ["Mystery", "Thriller"],
+    "runtime": "113 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYmQ3MjliNjAtNWFiZS00YWI1LTlmZTktMzBiNDE1NjRhZjU0XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Departed",
+    "year": 2006,
+    "cast": ["Leonardo DiCaprio", "Matt Damon", "Jack Nicholson"],
+    "director": "Martin Scorsese",
+    "genre": ["Crime", "Drama", "Thriller"],
+    "runtime": "151 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_SX300.jpg"
+  },
+  {
+    "title": "No Country for Old Men",
+    "year": 2007,
+    "cast": ["Tommy Lee Jones", "Javier Bardem", "Josh Brolin"],
+    "director": "Joel Coen, Ethan Coen",
+    "genre": ["Crime", "Drama", "Thriller"],
+    "runtime": "122 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Bourne Ultimatum",
+    "year": 2007,
+    "cast": ["Matt Damon", "Julia Stiles", "David Strathairn"],
+    "director": "Paul Greengrass",
+    "genre": ["Action", "Thriller"],
+    "runtime": "115 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYzE3ZGM4MzctZjU5MC00NWE2LTg5ZjYtMDFiM2ZlMWQ1MjkwXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Incredibles",
+    "year": 2004,
+    "cast": ["Craig T. Nelson", "Holly Hunter", "Samuel L. Jackson"],
+    "director": "Brad Bird",
+    "genre": ["Animation", "Action", "Adventure"],
+    "runtime": "115 min",
+    "certificate": "PG",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMTY5OTU0OTc2NV5BMl5BanBnXkFtZTcwMzU4MDcyMQ@@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Prestige",
+    "year": 2006,
+    "cast": ["Christian Bale", "Hugh Jackman", "Scarlett Johansson"],
+    "director": "Christopher Nolan",
+    "genre": ["Drama", "Mystery", "Sci-Fi"],
+    "runtime": "130 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjA4NDI0MTIxNF5BMl5BanBnXkFtZTYwNTM0MzY2._V1_SX300.jpg"
+  },
+  {
+    "title": "The Ring",
+    "year": 2002,
+    "cast": ["Naomi Watts", "Martin Henderson", "Brian Cox"],
+    "director": "Gore Verbinski",
+    "genre": ["Horror", "Mystery", "Thriller"],
+    "runtime": "115 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNDA2NTg2NjE4Ml5BMl5BanBnXkFtZTYwMjYxMDg5._V1_SX300.jpg"
+  },
+  {
+    "title": "Inception",
+    "year": 2010,
+    "cast": ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+    "director": "Christopher Nolan",
+    "genre": ["Action", "Adventure", "Sci-Fi"],
+    "runtime": "148 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Social Network",
+    "year": 2010,
+    "cast": ["Jesse Eisenberg", "Andrew Garfield", "Justin Timberlake"],
+    "director": "David Fincher",
+    "genre": ["Biography", "Drama"],
+    "runtime": "120 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjlkNTE5ZTUtNGEwNy00MGVhLThmZjMtZjU1NDE5Zjk1NDZkXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Mad Max: Fury Road",
+    "year": 2015,
+    "cast": ["Tom Hardy", "Charlize Theron", "Nicholas Hoult"],
+    "director": "George Miller",
+    "genre": ["Action", "Adventure", "Sci-Fi"],
+    "runtime": "120 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZDRkODJhOTgtOTc1OC00NTgzLTk4NjItNDgxZDY4YjlmNDY2XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Interstellar",
+    "year": 2014,
+    "cast": ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+    "director": "Christopher Nolan",
+    "genre": ["Adventure", "Drama", "Sci-Fi"],
+    "runtime": "169 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Revenant",
+    "year": 2015,
+    "cast": ["Leonardo DiCaprio", "Tom Hardy", "Domhnall Gleeson"],
+    "director": "Alejandro González Iñárritu",
+    "genre": ["Action", "Adventure", "Drama"],
+    "runtime": "156 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYTgwNmQzZDctMjNmOS00OTExLTkwM2UtNzJmOTJhODFjOTdlXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "La La Land",
+    "year": 2016,
+    "cast": ["Ryan Gosling", "Emma Stone", "John Legend"],
+    "director": "Damien Chazelle",
+    "genre": ["Comedy", "Drama", " Music"],
+    "runtime": "128 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SX300.jpg"
+  },
+  {
+    "title": "Get Out",
+    "year": 2017,
+    "cast": ["Daniel Kaluuya", "Allison Williams", "Catherine Keener"],
+    "director": "Jordan Peele",
+    "genre": ["Horror", "Mystery", "Thriller"],
+    "runtime": "104 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_SX300.jpg"
+  },
+  {
+    "title": "Parasite",
+    "year": 2019,
+    "cast": ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+    "director": "Bong Joon-ho",
+    "genre": ["Comedy", "Drama", "Thriller"],
+    "runtime": "132 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYjk1Y2U4MjQtY2ZiNS00OWQyLWI3MmYtZWUwNmRjYWRiNWNhXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Shape of Water",
+    "year": 2017,
+    "cast": ["Sally Hawkins", "Michael Shannon", "Richard Jenkins"],
+    "director": "Guillermo del Toro",
+    "genre": ["Adventure", "Drama", "Fantasy"],
+    "runtime": "123 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BOGFlMTM2MTgtZDdlMy00ZDZlLWFjOTUtZDMzMGEwNmNiMWY0XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Her",
+    "year": 2013,
+    "cast": ["Joaquin Phoenix", "Scarlett Johansson", "Amy Adams"],
+    "director": "Spike Jonze",
+    "genre": ["Drama", "Romance", "Sci-Fi"],
+    "runtime": "126 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMjA1Nzk0OTM2OF5BMl5BanBnXkFtZTgwNjU2NjEwMDE@._V1_SX300.jpg"
+  },
+  {
+    "title": "Apocalypse Now",
+    "year": 1979,
+    "cast": ["Martin Sheen", "Marlon Brando", "Robert Duvall"],
+    "director": "Francis Ford Coppola",
+    "genre": ["Drama", "War"],
+    "runtime": "153 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZDhiMTljYjYtODc1Yy00MmEwLTg2OTYtYmE1YTRmNDE4MmEwXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Platoon",
+    "year": 1986,
+    "cast": ["Charlie Sheen", "Tom Berenger", "Willem Dafoe"],
+    "director": "Oliver Stone",
+    "genre": ["Drama", "War"],
+    "runtime": "120 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZjE4YjllODAtZGMxZS00Mjg2LThkOWUtZjNkMjQ5YThiNDU4XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Thin Red Line",
+    "year": 1998,
+    "cast": ["Sean Penn", "Jim Caviezel", "Nick Nolte"],
+    "director": "Terrence Malick",
+    "genre": ["Drama", "War"],
+    "runtime": "170 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BMzQ5NmViMDUtMjI1NC00ZWJkLTkwNzctMmIxZWY2MzQyZDc2XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Full Metal Jacket",
+    "year": 1987,
+    "cast": ["Matthew Modine", "R. Lee Ermey", "Vincent D'Onofrio"],
+    "director": "Stanley Kubrick",
+    "genre": ["Drama", "War"],
+    "runtime": "116 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYWUzNzZkNzUtNDdiYy00Nzk5LTgxMmItNTk0MjRjNjdjNDA0XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Dunkirk",
+    "year": 2017,
+    "cast": ["Fionn Whitehead", "Tom Glynn-Carney", "Jack Lowden"],
+    "director": "Christopher Nolan",
+    "genre": ["Action", "Drama", "History"],
+    "runtime": "106 min",
+    "certificate": "PG-13",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BZWU5ZjJkNTQtMzQwOS00ZGE4LWJkMWUtMGQ5YjdiM2FhYmRhXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "1917",
+    "year": 2019,
+    "cast": ["George MacKay", "Dean-Charles Chapman", "Mark Strong"],
+    "director": "Sam Mendes",
+    "genre": ["Drama", "War"],
+    "runtime": "119 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYzkxZjg2NDQtMGVjMy00NWZkLTk0ZDEtZWE3NDYwYjAyMTg1XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "A Nightmare on Elm Street",
+    "year": 1984,
+    "cast": ["Heather Langenkamp", "Johnny Depp", "Robert Englund"],
+    "director": "Wes Craven",
+    "genre": ["Horror"],
+    "runtime": "91 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNTZmN2VlMWMtMTdlMi00NDBhLWIyODMtODEwMzM0ODMwMWYwXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "Poltergeist",
+    "year": 1982,
+    "cast": ["JoBeth Williams", "Craig T. Nelson", "Heather O'Rourke"],
+    "director": "Tobe Hooper",
+    "genre": ["Horror"],
+    "runtime": "114 min",
+    "certificate": "PG",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BNzY4MGZkYjgtYTQ2YS00YTlhLWEyMjAtMjZhYzczNTRlM2ZmXkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "The Thing",
+    "year": 1982,
+    "cast": ["Kurt Russell", "Wilford Brimley", "Keith David"],
+    "director": "John Carpenter",
+    "genre": ["Horror", "Sci-Fi"],
+    "runtime": "109 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BYTA3NDU5MWEtNTk4Yy00ZDNkLThmZTQtMjU3ZGVhYzAyMzU4XkEyXkFqcGc@._V1_SX300.jpg"
+  },
+  {
+    "title": "An American Werewolf in London",
+    "year": 1981,
+    "cast": ["David Naughton", "Jenny Agutter", "Joe Belcher"],
+    "director": "John Landis",
+    "genre": ["Horror", "Comedy"],
+    "runtime": "97 min",
+    "certificate": "R",
+    "poster_url": "https://m.media-amazon.com/images/M/MV5BOWYyNmMwMTUtZjE2OC00YWQzLWJmMzctNzhmMDI1OTQwNzdhXkEyXkFqcGc@._V1_SX300.jpg"
+  }
+]
