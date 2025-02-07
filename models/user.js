76bd312361d25abb5c486d5253b2c9404aef150c
Claire <clairebrady12@hotmail.com>
2025-02-07 16:56:03 +0000

    added user model

---
+++ models/user.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email address.'],
+    unique: true,
+    validate: {
+      message: "Please enter a valid email.",
+      validator: (email) => validator.isEmail(email)
+    },
+  },
+  username: {
+    type: String,
+    required: [true, 'Please provide a username.'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password.'],
+    validate: [
+      {
+        message: "Password must be at least 8 characters in length.",
+        validator: (password) => password.length >= 8
+      },
+      {
+        message: "Password must contain at least 1 lowercase, uppercase, and symbol",
+        validator: (password) => validator.isStrongPassword(password, 
+          { minLowercase: 1, minUppercase: 1, minSymbols: 1, minNumbers: 1 }
+        )
+      }
+    ]
+  }
+})
+
+userSchema.pre('save', function(next) {
+  
+  if (this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+  }
+  next() 
+})
+
+
+userSchema.methods.isPasswordValid = function(plainTextPassword){
+  const isValid = bcrypt.compareSync(plainTextPassword, this.password)
+  console.log(`Password is valid: ${isValid}`)
+  return isValid
+}
+
+
+const User = mongoose.model('User', userSchema)
+export default User
