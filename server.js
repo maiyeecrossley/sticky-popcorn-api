6a69d90bd0606373c8127584572c7c8452ca2fd2
Claire <clairebrady12@hotmail.com>
2025-02-10 09:28:40 +0000

    added user controller

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 import express from "express";
-import mongoose, { mongo } from "mongoose";
+import mongoose, {mongo} from "mongoose";
 import mongoSanitize from "express-mongo-sanitize";
 import "dotenv/config"
+
+
 import cors from "cors"
 
 import logger from "./middleware/logger.js"
 import errorHandler from "./middleware/errorHandler.js"
-
+import userController from './controllers/userController.js'
 const app = express()
-const port = process.env.port || 3000 //# use PORT env variable if it exists, otherwise use 3000
-
+const port = process.env.port || 3000
 app.use(cors())
 app.use(express.json()) //# parses JSON body type, adding them to the req.body
 app.use(mongoSanitize()) //# prevent cody injections
 app.use(logger) //# logs out key information on incoming requests
+app.use('/', userController)
 app.use(errorHandler)
 
 //? Server connection
