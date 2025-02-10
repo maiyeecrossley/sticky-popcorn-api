5de5d65352c6c8dda58cc54a705e472d769c8202
maiyeecrossley <maiyee.crossley@gmail.com>
2025-02-10 12:19:08 +0000

    added seeding for the reviews and users from database

--- seed.js
+++ seed.js
@@ -2,8 +2,12 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 
 import Movie from './models/movie.js'
+import Review from "./models/review.js"
+import User from "./models/user.js"
 
 import movies from './data/movies.js'
+import reviewData from "./data/reviews.js"
+import userData from "./data/users.js"
 
 const seedDatabase = async () => {
     try {
@@ -11,10 +15,29 @@ const seedDatabase = async () => {
       console.log('Database connected')
   
       // Clear DB
-      await Movie.deleteMany()      
+      await Movie.deleteMany()
+      await Review.deleteMany()
+      await User.deleteMany()      
       
       const newMovies = await Movie.create(movies)
       console.log(`${movies.length} posts created`)
+
+      const users = await User.create(userData)
+      console.log(`${users.length} users created`)
+
+      const movieMap = {}
+      movies.forEach(movie => {
+        movieMap[movie.title] = movie._id
+      })
+
+      const reviews = await Review.create(reviewData.map(review => ({
+        content: review.content,
+        author: users[Math.floor(Math.random() * users.length)]._id,
+        movie: movieMap[review.title] || newMovies[0]._id
+      })))
+
+      console.log(`${reviews.length} reviews added`)
+
   
       await mongoose.connection.close()
       console.log('Database connection closed')
