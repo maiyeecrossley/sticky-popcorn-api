20a6660cf2aff7537e79485123ac6f4399bf4e9c
Kamran <kamran.raja@hotmail.co.uk>
2025-03-14 12:09:18 +0000

    reinstalled cors

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-import express from "express";
-import mongoose, {mongo} from "mongoose";
-import mongoSanitize from "express-mongo-sanitize";
+import express from "express"
+import mongoose, {mongo} from "mongoose"
+import mongoSanitize from "express-mongo-sanitize"
 import "dotenv/config"
-
-
 import cors from "cors"
 
 import logger from "./middleware/logger.js"
 import errorHandler from "./middleware/errorHandler.js"
 
-import userController from './controllers/userController.js'
-
-
 // Controllers/Routers
+import userController from './controllers/userController.js'
 import movieController from './controllers/movieController.js'
 import reviewController from "./controllers/reviewController.js"
 
 
 const app = express()
 const port = process.env.port || 3000
+
+// CORS Configuration
 app.use(cors())
+
 app.use(express.json()) //# parses JSON body type, adding them to the req.body
 app.use(mongoSanitize()) //# prevent cody injections
 app.use(logger) //# logs out key information on incoming requests
-app.use('/', userController)
-app.use(errorHandler)
 
 // Controllers / Routes
+app.use('/', userController)
 app.use('/', movieController)
 app.use('/', reviewController)
+
+app.use(errorHandler)
 
 //? Server connection
 const establishServerConnections = async () => {
